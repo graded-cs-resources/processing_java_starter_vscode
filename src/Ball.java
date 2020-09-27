88871bfce7d3e39973ddb51dc0009102be3ee9f9
David <david.griswold@graded.br>
2020-09-26 21:32:31 -0300

    Javadocs enabled

--- src/Ball.java
+++ src/Ball.java
@@ -2,39 +2,64 @@ import processing.core.PApplet;
 
 class Ball {
 
+    /** A link to the sketch that is making the balls */
     private PApplet sketch;
-    // instance variables with default values
-    private int size = 100; // diameter of the ball
-    private int x = 100; // The horizontal position of the ball (bigger => right)
-    private int y = 50; // the vertical position of the ball (bigger => down)
-    private int col; // set default in constructor
 
-    /** These represent the intial movement speed of the balls **/
+    /** diameter of the ball */
+    private int size = 100;
+
+    /** horizontal position of the ball */
+    private int x = 100;
+
+    /** vertical position of the ball */
+    private int y = 50; // the vertical position of the ball (bigger => down)
+
+    /** color of the ball, defaults to black */
+    private int col = 0;
+
+    /** speed of the ball in the Y direction (positive is down) */
     private int speedY = 5;
+
+    /** speed of the ball in the X direction (positive is right) */
     private int speedX = 1;
 
-    // default constructor just leave sth edefault values
+    /**
+     * Default constructor, leaves all default options
+     * 
+     * @param s A link to the sketch that is making the ball
+     */
     public Ball(PApplet s) {
         sketch = s;
-        col = sketch.color(255, 0, 0);
     }
 
+    /**
+     * Constructor that sets the position and size of your {@link Ball}
+     * 
+     * @param s        A link to the sketch that is making the ball
+     * @param theX     x-coordinate of the center of the ball
+     * @param theY     y-coordinate of the center of the ball
+     * @param ballSize diameter of the ball
+     */
     public Ball(PApplet s, int theX, int theY, int ballSize) {
         sketch = s;
-        col = sketch.color(255, 0, 0);
         x = theX;
         y = theY;
         size = ballSize;
     }
 
-    public Ball(PApplet s, int theX, int theY, int ballSize, int ballColor) {
-        sketch = s;
-        x = theX;
-        y = theY;
-        size = ballSize;
-        col = ballColor;
-    }
-
+    /**
+     * Constructor that sets the position and size of your {@link Ball}
+     * 
+     * @param s         A link to the sketch that is making the ball
+     * @param theX      x-coordinate of the center of the ball
+     * @param theY      y-coordinate of the center of the ball
+     * @param ballSize  diameter of the ball
+     * @param ballColor color of the ball as an int - use color(red, green, blue) to
+     *                  make this!
+     * @param sx        speed in the x-direction (how many pixels per 60th of a
+     *                  second) - positive is right
+     * @param sy        speed in the y-direction - positive is down
+     */
     public Ball(PApplet s, int theX, int theY, int ballSize, int ballColor, int sx, int sy) {
         sketch = s;
         x = theX;
@@ -45,22 +70,23 @@ class Ball {
         speedY = sy;
     }
 
-    /** Draws the ball. DO NOT EDIT. */
+    /** Draws the ball. */
     public void drawBall() {
         sketch.stroke(col);
         sketch.fill(col);
         sketch.ellipse(x, y, size, size);
     }
 
-    /** Moves the balls. DO NOT EDIT. */
+    /** Moves the balls. */
     public void moveBall() {
+        /* If the ball is on the edge, flip the direction. Bounce! */
         if (x > sketch.width - size / 2 || x < size / 2) {
             speedX = -speedX;
         }
         if (y > sketch.height - size / 2 || y < size / 2) {
             speedY = -speedY;
         }
-
+        /* Add the speed in both directions to move the ball */
         x += speedX;
         y += speedY;
     }
--- src/Sketch.java
+++ src/Sketch.java
@@ -1,36 +1,60 @@
-import processing.core.*;
+import processing.core.PApplet;
 
+/**
+ * This class defines the Processing Sketch It extends PApplet, which is a
+ * special processing class to represent the sketches
+ * 
+ * https://processing.org/reference/ is a helpful site to help you better
+ * understad the built-in processing functions
+ */
 public class Sketch extends PApplet {
-    Ball b1 = new Ball(this);
-    Ball b2 = new Ball(this, 200, 100, 50);
-    Ball b3 = new Ball(this, 50, 150, 20, color(0, 255, 0));
-    Ball b4 = new Ball(this, 300, 300, 150, color(0, 0, 255), -8, -1);
+    // In these three variables, `this` is referring to the object this code
+    // is inside! In this case, that is the sketch we have created.
 
+    /** First ball is the default black ball. */
+    Ball b1 = new Ball(this);
+
+    /** Second ball customizes size and position, but not color or speed */
+    Ball b2 = new Ball(this, 200, 100, 50);
+
+    /** Third ball defines all the parameters */
+    Ball b3 = new Ball(this, 300, 300, 150, color(0, 0, 255), -8, -1);
+
+    /**
+     * This function runs at the very beginning and defines the settings of the
+     * sketch. Only certain methods are allowed in here!
+     */
     public void settings() {
         size(500, 500);
     }
 
+    /**
+     * This function runs after setup, and initializes your code. You can put
+     * whatever you want in here, but it only runs once at the beginning!
+     */
     public void setup() {
-
+        System.out.println(Integer.toHexString(color(160, 160, 160, 160)));
     }
 
+    /**
+     * This function runs over and over and over, approximately 60 times per second!
+     */
     public void draw() {
-        background(0);
+        // paint the background gray
+        background(100);
 
+        // draw all three balls
         b1.drawBall();
         b2.drawBall();
         b3.drawBall();
-        b4.drawBall();
 
-        /******************************/
-
+        // tell all three balls to move so they are in a different place next time
         b1.moveBall();
         b2.moveBall();
         b3.moveBall();
-        b4.moveBall();
-
     }
 
+    /** All processing sketches have to use this main method */
     public static void main(String[] args) {
         PApplet.main("Sketch");
     }
